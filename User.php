921d9933e968ddce02c7a4799cd68e855e23d309
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-15 16:02:26 +0100

    metodo

--- User.php
+++ User.php
@@ -5,15 +5,29 @@ class User{
     public $password;
     public $eta;
     public $status;
+    public $bonus = 0;
     
 // dichiaro costruttore
     public function __construct($_nickname = ' '){
         $this->nickname = $_nickname;
         $this->status = $_status;
-        
+    }
+    public function setBonus($status){
+        if($status == 'advanced'){
+            $this->bonus = 100;
+        }
+        elseif($status == 'intermediate'){
+            $this->bonus = 50;
+        }
+        else {
+            $this->bonus = 20;
+        }
+    }
+    public function getBonus(){
+        return $this->bonus;
     }
 }
-// istanzio 3 nuovi user con un nickname
+// istanzio 3 nuovi user con un nickname e uno status
 $hackerman = new User();
 $pokerboy = new User();
 $funnyguy = new User();
@@ -40,15 +54,21 @@ echo $hackerman->password;
 echo $hackerman->eta;
 echo $hackerman->status;
 
+
 echo $pokerboy->nickname;
 echo $pokerboy->password;
 echo $pokerboy->eta;
 echo $pokerboy->status;
 
+
 echo $funnyguy->nickname;
 echo $funnyguy->password;
 echo $funnyguy->eta;
 echo $funnyguy->status;
+
+
+
+
 
 
 

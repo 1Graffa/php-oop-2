3048657df96e55eb02bcec355e35ada1faa9975d
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-15 15:40:10 +0100

    correzioni

--- User.php
+++ User.php
@@ -2,8 +2,8 @@
 
 class User{
     public $nickname;
-    private $password;
-    private $eta;
+    public $password;
+    public $eta;
     public $status;
     
 // dichiaro costruttore
@@ -12,7 +12,6 @@ class User{
         $this->status = $_status;
         
     }
-
 }
 // istanzio 3 nuovi user con un nickname
 $hackerman = new User();
@@ -21,14 +20,35 @@ $funnyguy = new User();
 
 // accedo agli oggetti istanziati
 $hackerman->nickname = 'hackerman99 ';
-$pokerboy->nickname = 'pokerboy79 ';
-$funnyguy->nickname = 'funnyguy88 ';
+$hackerman->password = '123stella ';
+$hackerman->eta = '30';
+$hackerman->status = 'advanced';
 
+$pokerboy->nickname = 'pokerboy79 ';
+$pokerboy->password = '123palla ';
+$pokerboy->eta = '25';
+$pokerboy->status = 'intermediate';
+
+$funnyguy->nickname = 'funnyguy88 ';
+$funnyguy->password = '123opla ';
+$funnyguy->eta = '19';
+$funnyguy->status = 'beginner';
 
 // stampo a schermo
 echo $hackerman->nickname;
+echo $hackerman->password;
+echo $hackerman->eta;
+echo $hackerman->status;
+
 echo $pokerboy->nickname;
+echo $pokerboy->password;
+echo $pokerboy->eta;
+echo $pokerboy->status;
+
 echo $funnyguy->nickname;
+echo $funnyguy->password;
+echo $funnyguy->eta;
+echo $funnyguy->status;
 
 
 
